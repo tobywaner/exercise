259deeb3cbd200821af677647d47ed7e6c67fbe9
笑 王 <tobywaner@hotmail.com>
2018-08-08 22:05:57 +0800

    microblog exercise day 2

--- Flask/microblog/app/views.py
+++ Flask/microblog/app/views.py
@@ -1,8 +1,23 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'nickname': 'Miguel' } # fake user
+    posts = [ # fake array of posts
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
 
