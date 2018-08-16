9aa5bf31016c91768a5a30a51d633fd7d7f9a695
笑 王 <tobywaner@hotmail.com>
2018-08-16 19:33:01 +0800

    Add student and depart's url rule

--- Signup/app/views.py
+++ Signup/app/views.py
@@ -2,52 +2,73 @@ from app import app
 from flask import render_template, request
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
     return render_template("base.html")
 
 
-@app.route('/results')
+@app.route("/results")
 def show_results():
-    uuid = request.args.get('suid')
+    uuid = request.args.get("suid")
     return render_template("results.html", recruit_uuid=uuid)
 
 
-@app.route('/backoffice')
-@app.route('/login')
-@app.route('/backoffice/student')
-@app.route('/backoffice/recruit')
-@app.route('/backoffice/depart')
+@app.route("/backoffice")
+@app.route("/login")
+@app.route("/backoffice/student")
+@app.route("/backoffice/recruit")
+@app.route("/backoffice/depart")
 def login():
     return render_template("login.html")
 
 
-@app.route('/backoffice/recruit/add')
+@app.route("/backoffice/recruit/add")
 def recruit_add():
     return render_template("recruit/add.html")
 
 
-@app.route('/backoffice/recruit/list')
+@app.route("/backoffice/recruit/list")
 def recruit_list():
     return render_template("recruit/list.html")
 
 
-@app.route('/backoffice/recruit/modify/<recruit_uuid>')
+@app.route("/backoffice/recruit/modify/<recruit_uuid>")
 def recruit_update(recruit_uuid):
     return render_template("recruit/modify.html", recruit_uuid=recruit_uuid)
 
 
-@app.route('/backoffice/recruit/detail/<recruit_uuid>')
+@app.route("/backoffice/recruit/detail/<recruit_uuid>")
 def recruit_show(recruit_uuid):
     return render_template("recruit/detail.html", recruit_uuid=recruit_uuid)
 
-# / backoffice / recruit / list
-# / backoffice / recruit / add
-# / backoffice / recruit / modify?suid = xxxx - xx - xx - xxxx
-# / backoffice / recruit / detail?suid = xxxx - xx - xx - xxxx
-# / backoffice / student / add?suid = xxxx - xx - xx - xxxx
-# / backoffice / student / detail?suid = xxxx - xx - xx - xxxx & stid = xxxx - xx - xx - xxxx
-# / backoffice / depart / add
-# / backoffice / depart / list
-# / backoffice / depart / modify?dtid = xxxx - xx - xx - xxxx
+
+@app.route("/backoffice/recruit/<recruit_uuid>/student/add")
+def student_add(recruit_uuid):
+    return render_template("student/add.html", recruit_uuid=recruit_uuid)
+
+
+@app.route("/backoffice/recruit/<recruit_uuid>/student/add")
+def student_add(recruit_uuid):
+    return render_template("student/add.html", recruit_uuid=recruit_uuid)
+
+
+@app.route("/backoffice/recruit/<recruit_uuid>/student/detail/<student_id>")
+def student_show(recruit_uuid, student_id):
+    return render_template("student/detail.html", recruit_uuid=recruit_uuid, student_id=student_id)
+
+
+@app.route("/backoffice/depart/add")
+def depart_add():
+    return render_template("depart/add.html")
+
+
+@app.route("/backoffice/depart/list")
+def depart_list():
+    return render_template("depart/list.html")
+
+
+@app.route("/backoffice/depart/modify/<depart_uuid>")
+def depart_update(depart_uuid):
+    return render_template("depart/modify.html", depart_uuid=depart_uuid)
+
